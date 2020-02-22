5d8653b304946048c0ea1ec1d4da45f8cb313011
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-02-22 09:48:01 +0530

    using chalk package

--- learn-npm-package/app.js
+++ learn-npm-package/app.js
@@ -1,9 +1,12 @@
 const validator = require('validator');
+const chalk = require('chalk');
+const log = chalk.blueBright.bold;
+
 
 const isEmail = validator.isEmail('hell@gmail.com')
 
 const isUrl = validator.isURL('https://www.google.com')
 
-console.log('Is an Email : ' , isEmail)
-console.log('Is an URL   : ' , isUrl)
+console.log('Is an Email : ' , log(isEmail))
 
+console.log('Is an URL   : ', log(isUrl));
