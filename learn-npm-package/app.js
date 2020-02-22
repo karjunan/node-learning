67ffdba55018affcdfd15da05e7adc3042acf7c3
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-02-22 10:17:52 +0530

    process environment learning

--- learn-npm-package/app.js
+++ learn-npm-package/app.js
@@ -10,3 +10,7 @@ const isUrl = validator.isURL('https://www.google.com')
 console.log('Is an Email : ' , log(isEmail))
 
 console.log('Is an URL   : ', log(isUrl));
+console.log(chalk.bold('nodemon is working Mate !!'))
+
+console.log('arg 2 : ', process.argv[2])
+console.log('arg 1 : ', process.argv[1])
