69bef0ba08bcf716465d1736d15e213e5f78b8a1
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-02-22 10:36:45 +0530

    yargs

--- learn-npm-package/app.js
+++ learn-npm-package/app.js
@@ -12,5 +12,6 @@ console.log('Is an Email : ' , log(isEmail))
 console.log('Is an URL   : ', log(isUrl));
 console.log(chalk.bold('nodemon is working Mate !!'))
 
+
 console.log('arg 2 : ', process.argv[2])
 console.log('arg 1 : ', process.argv[1])
---
+++ learn-process/command.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk')
+const validator = require('validator')
+const yargs = require('yargs')
+
+console.log(process.argv)
+console.log(yargs.argv)
+
+console.log(yargs.argv.title)
