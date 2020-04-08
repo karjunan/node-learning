acb7778b6931790bd139240862f009a9395ad5d2
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-04-08 09:52:03 +0530

    imported mongo documents

--- book-app/app.js
+++ book-app/app.js
@@ -1,13 +1,23 @@
-var express = require('express')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+const mongoose = require('mongoose');
 
-var app = express();
+const db = mongoose.connect('mongodb://localhost/test')
 
-var port = process.env.PORT || 3000;
+bookRouter.route('/books')
+    .get((req,res) => {
+      const response = {'hello' : "this is my response"}
+      res.json(response);
+    })
 
-app.get('/', (req,res) => {
-    res.send("Welcome to my api");
-})
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my api !! Coo');
+});
 
 app.listen(port, () => {
-    console.log("Listening to port " , port);
-})
+  console.log(`Listening to port ${port}`);
+});
