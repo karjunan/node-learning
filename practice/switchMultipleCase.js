21fc5c628825fc2fc59f1d518cd262e2b11f6009
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-05-30 07:43:17 +0530

    switch statement

---
+++ practice/switchMultipleCase.js
@@ -0,0 +1,17 @@
+function multipleCase(input) {
+
+    switch(input) {
+        case 'red' :
+            console.log("Red color"); 
+        case 'blue' :
+            console.log("Blue color");
+        case 'purple':
+            break;
+        case 'green':
+            console.log("green");
+            break;       
+    }
+}
+
+let color ='red';
+multipleCase(color)
---
+++ practice/switchStmt.js
@@ -0,0 +1,24 @@
+function swtichStms(input) {
+
+    switch(input) {
+        case 1: 
+            console.log("Case 1");
+            break;
+        case 2:
+            console.log("Case 2");
+            break;
+        case 3:
+            console.log("Case 3")
+            break;
+        case 1:
+            console.log("Case 1 and its string");
+            break;
+        default:
+            console.log("This is the default");
+            break;
+    }
+
+}
+
+swtichStms(1);
+swtichStms("1");
